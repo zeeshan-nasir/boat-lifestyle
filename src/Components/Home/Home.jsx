aa313807c41fc2d9c076a5c290b52207ddd827f1
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-05-04 19:53:17 +0530

    added some data to front page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../Styles/home.css";
 import { CarouselDiv } from "./Carousel";
 import TopSellers from "./TopSellers";
+import TrendingEarphones from "./TrendingEarphones";
 
 const Home = () => {
    return (
       <div>
          <CarouselDiv />
          <TopSellers />
+         <TrendingEarphones />
       </div>
    );
 };
--- src/Components/Home/TrendingEarphones.jsx
+++ src/Components/Home/TrendingEarphones.jsx
@@ -1,50 +1,56 @@
 import React, { useState } from "react";
 import { AiFillStar } from "react-icons/ai";
 
-const TopSellers = () => {
+const TrendingEarphones = () => {
    const [useClass, setUseClass] = useState("first");
 
    return (
       <div className="topSellersDiv">
-         <h1 className="headingText">Top Sellers</h1>
+         <h1 className="headingText">Top Earphones</h1>
          <div className="textDiv flex">
             <p
                onClick={() => setUseClass("first")}
                className={useClass === "first" ? "red" : "simple"}
             >
-               Smart Watches
+               Earbuds
             </p>
             <p
                onClick={() => setUseClass("second")}
                className={useClass === "second" ? "red" : "simple"}
             >
-               Top Picks
+               Wireless
+            </p>
+            <p
+               onClick={() => setUseClass("third")}
+               className={useClass === "third" ? "red" : "simple"}
+            >
+               Wired
             </p>
          </div>
          <div className="cardsDiv flex">
             <div className="card">
                <div className="imageDiv">
                   <img
-                     src="https://cdn.shopify.com/s/files/1/0057/8938/4802/products/xtend_black_400x.png?v=1650387008"
+                     src="https://cdn.shopify.com/s/files/1/0057/8938/4802/products/218997d1-3fe9-4a19-809c-94a8a2d05273_500x.png?v=1625045659"
                      alt=""
                   />
                </div>
                <div className="dataDiv">
                   <p className="reviewsDiv">
                      <AiFillStar />
-                     4.68 (34 reviews)
+                     4.73 (114 reviews)
                   </p>
-                  <p className="title">boAt Watch Xtend‌</p>
+                  <p className="title">BOAT Airdopes 441 Pro - Bluetooth</p>
                   <div className="priceDiv flex">
                      <div className="flex">
-                        <p className="price">₹ 2,499</p>
-                        <p className="strPrice">₹ 7,990</p>
+                        <p className="price">₹ 2,299</p>
+                        <p className="strPrice">₹ 5,999</p>
                      </div>
                      <button className="cardBtn">ADD +</button>
                   </div>
                   <ul className="list">
-                     <li>Colour variants for every style</li>
-                     <li>Fast Charge in just 15 minutes</li>
+                     <li>Thumping bass & immersive sound</li>
+                     <li>Playback of 5H with each charge</li>
                      <li>Ace your workouts with IPX5 rating</li>
                   </ul>
                </div>
@@ -52,72 +58,46 @@ const TopSellers = () => {
             <div className="card">
                <div className="imageDiv">
                   <img
-                     src="https://cdn.shopify.com/s/files/1/0057/8938/4802/products/a6549acb-b075-4c3e-8ed3-9c9fcba32d42_600x.png?v=1625046216"
+                     src="https://cdn.shopify.com/s/files/1/0057/8938/4802/products/402-main-img_400x.png?v=1630059242"
                      alt=""
                   />
                </div>
                <div className="dataDiv">
                   <p className="reviewsDiv">
                      <AiFillStar />
-                     4.79 (757 reviews)
+                     4.76 (386 reviews)
                   </p>
-                  <p className="title">boAt Storm</p>
+                  <p className="title">boAt Airdopes 402 - Wireless Earbuds</p>
                   <div className="priceDiv flex">
                      <div className="flex">
-                        <p className="price">₹ 2,499</p>
+                        <p className="price">₹ 1,599</p>
                         <p className="strPrice">₹ 5,990</p>
                      </div>
                      <button className="cardBtn">ADD +</button>
                   </div>
                   <ul className="list">
-                     <li>Track and transform your health</li>
-                     <li>Full touch display</li>
-                     <li>Track your SpO2 levels</li>
+                     <li>Ergonomic design</li>
+                     <li>Uninterrupted music for 4 hours</li>
+                     <li>Pocket-friendly charging case</li>
                   </ul>
                </div>
             </div>
             <div className="card">
                <div className="imageDiv">
                   <img
-                     src="https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main-blue_d6503e2e-e0b1-4c4e-af28-2164903baf9f_400x.png?v=1640237576"
+                     src="https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main3_6e1b5a7a-8f56-48c6-819d-24395393db81_400x.png?v=1624430639"
                      alt=""
                   />
                </div>
                <div className="dataDiv">
                   <p className="reviewsDiv">
                      <AiFillStar />
-                     4.68 (34 reviews)
+                     4.83 (6 reviews)
                   </p>
-                  <p className="title">boAt Watch Iris</p>
+                  <p className="title">BOAT Airdopes 281 Pro</p>
                   <div className="priceDiv flex">
                      <div className="flex">
-                        <p className="price">₹ 4,399</p>
-                        <p className="strPrice">₹ 11,999</p>
-                     </div>
-                     <button className="cardBtn">ADD +</button>
-                  </div>
-                  <ul className="list">
-                     <li>Amoled display</li>
-                     <li>1.39 inches display</li>
-                     <li>Health on the go</li>
-                  </ul>
-               </div>
-            </div>
-            <div className="card">
-               <div className="imageDiv">
-                  <img
-                     src="https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main_blue_295a504a-2ae2-4719-9400-263f453efddd_400x.png?v=1635140773"
-                     alt=""
-                  />
-               </div>
-               <div className="dataDiv">
-                  <p className="reviewsDiv">
-                     <AiFillStar />5 (2 reviews)
-                  </p>
-                  <p className="title">boAt Watch‌ Vertex</p>
-                  <div className="priceDiv flex">
-                     <div className="flex">
-                        <p className="price">₹ 2,399</p>
+                        <p className="price">₹ 2,499</p>
                         <p className="strPrice">₹ 6,990</p>
                      </div>
                      <button className="cardBtn">ADD +</button>
@@ -129,9 +109,35 @@ const TopSellers = () => {
                   </ul>
                </div>
             </div>
+            <div className="card">
+               <div className="imageDiv">
+                  <img
+                     src="https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main-img_AD621_1_400x.png?v=1616230788"
+                     alt=""
+                  />
+               </div>
+               <div className="dataDiv">
+                  <p className="reviewsDiv">
+                     <AiFillStar />5 (2 reviews)
+                  </p>
+                  <p className="title">BOAT Airdopes 621</p>
+                  <div className="priceDiv flex">
+                     <div className="flex">
+                        <p className="price">₹ 2,499</p>
+                        <p className="strPrice">₹ 7,990</p>
+                     </div>
+                     <button className="cardBtn">ADD +</button>
+                  </div>
+                  <ul className="list">
+                     <li>Perfectly fits ears for dance sessions</li>
+                     <li>Enjoy 3.5 hours of unstoppable music</li>
+                     <li>IPX5 rated for workouts and adventures</li>
+                  </ul>
+               </div>
+            </div>
          </div>
       </div>
    );
 };
 
-export default TopSellers;
+export default TrendingEarphones;

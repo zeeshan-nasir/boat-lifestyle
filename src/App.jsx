20c277ace8bafe5007e0ac9f01cfbad1ce617a6f
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-05-06 19:15:25 +0530

    added login and registered functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import { Products } from "./Components/Products/Products";
-
+import  {Login } from "./Components/login/Login";
+import { Register } from "./Components/login/Register";
 function App() {
    return (
       <div className="App">
@@ -13,6 +14,8 @@ function App() {
          <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/products" element={<Products/>}></Route>
+            <Route path="/account/login" element={<Login />}></Route>
+            <Route path="/account/register" element = {<Register />}></Route>
          </Routes>
          <Footer />
       </div>

aed1d9d7364c24c6bb954ecfeea93b36ea80d3da
Gaurav Arsod <gaarsod14@gmail.com>
2022-05-06 20:54:54 +0530

    updates

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState} from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSliders } from "@fortawesome/free-solid-svg-icons";
 
 export const Products = () => {
     const [products, setProducts] = useState([]);
-
+    const [img, setImage] = useState();
     
     useEffect(() => {
 
@@ -11,7 +13,7 @@ export const Products = () => {
     }, []);
 
     const getData = () => {
-        fetch("http://localhost:5000/products")
+        fetch("http://localhost:8080/products")
         .then((res) => res.json())
         .then(data => {
             setProducts(data)
@@ -24,6 +26,10 @@ export const Products = () => {
         // }
     }
 
+    // const imageSetter =(img) =>{
+    //     setImage(img)
+    // }
+
     console.log(products)
 
     return (
@@ -31,34 +37,44 @@ export const Products = () => {
             <img className="topImage" src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Rockerz-_Catgry_web_d201c6e3-e067-4280-a127-56a2e1b51399.jpg?v=1634806683" alt="" />
 
             <div className="main">
-                <h1>Wireless Headphones</h1>
+                <h1 className="heading">Wireless Headphones</h1>
 
             <div className="filterAndSort">
                 <div>
+                    <div className="filter">
+                        <FontAwesomeIcon icon={faSliders} />
                     <h3>Show Filters</h3>
+                    </div>
                     <h3>{products.length} products</h3>
                 </div>
 
-                <select id="sort">
-                    <option value="featured">Featured</option>
-                    <option value="lowToHigh">Price, Low to High</option>
-                    <option value="highToLow">Price, High to Low</option>
-                    <option value="A-Z">Alphabetically, A-Z</option>
-                    <option value="Z-A">Alphabetically, Z-A</option>
-                </select>
+                <div className="sortDiv">
+                    <h3>Sort by</h3>
+                    <select id="sort">
+                        <option className="opt" value="featured">Featured</option>
+                        <option className="opt" value="lowToHigh">Price, Low to High</option>
+                        <option className="opt" value="highToLow">Price, High to Low</option>
+                        <option className="opt" value="A-Z">Alphabetically, A-Z</option>
+                        <option  className="opt" value="Z-A">Alphabetically, Z-A</option>
+                    </select>
+                </div>
             </div>
 
             <div className="products_container">
                 {products.map((ele) => {
+                    // {imageSetter(ele.img)}
                     return (
                         <div className="productCard">
-                            <img src={ele.img} alt="" />
+                            <img  src={ele.img} alt="Sorry unable to picture" />
                             <div className="productInfo">
                                 <h5>{ele.rating} ({ele.total_rating})</h5>
                                 <h4>{ele.title}</h4>
                                 <div className="price">
-                                    <h4>₹ {ele.price}</h4>
+                                    <div>
+                                        <h4>₹ {ele.price}</h4>
                                     <h6>₹ {ele.original_price}</h6>
+                                    </div>
+                                    <button>Add +</button>
                                 </div>
 
                                 <h5>{ele.colors} colors available</h5>

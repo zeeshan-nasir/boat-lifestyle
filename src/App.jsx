9924c39fc4c23197d440224fb38eb71e0d6249c8
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-05-06 19:45:53 +0530

    Revert "added login and registered functionality"
    This reverts commit 20c277ace8bafe5007e0ac9f01cfbad1ce617a6f.

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,6 @@ import Checkout from "./Components/Payments/Checkout";
 import Payments from "./Components/Payments/Payments";
 import Otp from "./Components/Payments/Otp";
 
-import  {Login } from "./Components/login/Login";
-import { Register } from "./Components/login/Register";
 function App() {
    return (
       <div className="App">
@@ -21,9 +19,7 @@ function App() {
             <Route path="/checkout" element={<Checkout />}></Route>
             <Route path="/payments" element={<Payments />}></Route>
             <Route path="/otp" element={<Otp />}></Route>
-            <Route path="/products" element={<Products/>}></Route>
-            <Route path="/account/login" element={<Login />}></Route>
-            <Route path="/account/register" element = {<Register />}></Route>
+            <Route path="/products" element={<Products />}></Route>
          </Routes>
          <Footer />
       </div>

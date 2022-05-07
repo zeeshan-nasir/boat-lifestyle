a247260047a9c267c37c36aeeeb84890c873ca2e
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-05-07 20:18:36 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,13 @@
 
 const app = require("./index");
 const connect = require("./configs/db");
+require("dotenv").config();
 
-app.listen(5000, async () => {
-  try {
-    await connect();
-    console.log("Connection established on port 5000!");
-  } catch (err) {
-    console.log("Error connecting!", err);
-  }
+app.listen(process.env.PORT || 5000, async () => {
+   try {
+      await connect();
+      console.log("Connection established on port 5000!");
+   } catch (err) {
+      console.log("Error connecting!", err);
+   }
 });

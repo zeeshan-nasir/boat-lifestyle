6b92856ddeffdea0401db4075adfae87d5a88a3c
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-05-08 18:36:38 +0530

    Revert "Update server.js"
    This reverts commit a247260047a9c267c37c36aeeeb84890c873ca2e.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,12 @@
 
 const app = require("./index");
 const connect = require("./configs/db");
-require("dotenv").config();
 
-app.listen(process.env.PORT || 5000, async () => {
-   try {
-      await connect();
-      console.log("Connection established on port 5000!");
-   } catch (err) {
-      console.log("Error connecting!", err);
-   }
+app.listen(5000, async () => {
+  try {
+    await connect();
+    console.log("Connection established on port 5000!");
+  } catch (err) {
+    console.log("Error connecting!", err);
+  }
 });
